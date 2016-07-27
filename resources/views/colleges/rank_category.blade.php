dbc45aae0ee682ff9ca5a3276e7d1847e3913dcf
ChengLei Shao <chareice@live.com>
2016-07-27 17:49:16 +0800

    update college rank

--- resources/views/colleges/rank_category.blade.php
+++ resources/views/colleges/rank_category.blade.php
@@ -1,6 +1,13 @@
+@if($level == 1)
+<span style="font-weight: bold" class="level-{{$level}}">{{ $category['name'] }}</span>
+@else
 <a
   href="{{route('colleges.rank', ['category_id' => $category['_id']])}}"
-  class="level-{{$level}} @if($category['_id'] == $selected_category_id) active @endif" >{{ $category['name'] }}</a>
+  class="level-{{$level}} @if($category['_id'] == $selected_category_id) active @endif" >
+  {{ $category['name'] }}
+</a>
+@endif
+
 @if (count($category['children']) > 0)
     @foreach($category['children'] as $category)
         @include('colleges.rank_category', ['category' => $category, 'level' => $level + 1, 'selected_category_id' => $selected_category_id])
