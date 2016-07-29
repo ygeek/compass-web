6f25c4b3674be225c514873c37616f943a6bb9b3
ChengLei Shao <chareice@live.com>
2016-07-29 13:51:01 +0800

    排行榜分页样式

--- resources/views/colleges/rank.blade.php
+++ resources/views/colleges/rank.blade.php
@@ -25,35 +25,39 @@
                 @endforeach
             </div>
 
-            <table class="colleges">
-                @foreach($colleges as $college)
-                    <tr>
-                    <td class="rank">{{ $college['rank'] }}</td>
-                    <td class="chinese_name">
-                      @if($college['key'])
-                        <a style="color:#000" target="_blank" href="{{route('colleges.show', ['key' => \App\College::generateKey($college['key']) ])}}">
-                          {{ $college['chinese_name'] }}
-                        </a>
-                      @else
-                        {{ $college['chinese_name'] }}
-                      @endif
-                    </td>
-                    <td class="english_name">{{ $college['english_name'] }}</td>
-                    @if(isset($college['world_ranking']))
-                    <td class="english_name">世界排名: {{$college['world_ranking']}}</td>
-                    @endif
-                    <td class="actions">@if($college['key'])<a href="{{ route('estimate.step_first') }}"><button class="estimate-button">测试录取率 -></button></a>@endif</td>
-                    </tr>
-                @endforeach
-            </table>
+            <div class="rank-content">
+                <table class="colleges">
+                    @foreach($colleges as $college)
+                        <tr>
+                        <td class="rank">{{ $college['rank'] }}</td>
+                        <td class="chinese_name">
+                          @if($college['key'])
+                            <a style="color:#000" target="_blank" href="{{route('colleges.show', ['key' => \App\College::generateKey($college['key']) ])}}">
+                              {{ $college['chinese_name'] }}
+                            </a>
+                          @else
+                            {{ $college['chinese_name'] }}
+                          @endif
+                        </td>
+                        <td class="english_name">{{ $college['english_name'] }}</td>
+                        @if(isset($college['world_ranking']))
+                        <td class="english_name">世界排名: {{$college['world_ranking']}}</td>
+                        @endif
+                        <td class="actions">@if($college['key'])<a href="{{ route('estimate.step_first') }}"><button class="estimate-button">测试录取率 -></button></a>@endif</td>
+                        </tr>
+                    @endforeach
+                </table>
+
+                <div class="pagination">
+                  {{ $colleges->appends(app('request')->except('page'))->render() }}
+                </div>
+            </div>
+
             <?php unset($college) ?>
             <div style="padding-top: 20px;">
               @include('colleges.sidebar')
             </div>
 
-            <div style="padding-left: 250px;">
-              {{ $colleges->appends(app('request')->except('page'))->render() }}
-            </div>
 
         </div>
     </div>
