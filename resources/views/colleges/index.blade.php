1db7544a3d14beb9765686913c913af53ed914a7
Culiner <295281473@qq.com>
2016-08-08 18:06:55 +0800

    院校列表前端修改

--- resources/views/colleges/index.blade.php
+++ resources/views/colleges/index.blade.php
@@ -67,7 +67,7 @@
 
                                     <div class="ielts-and-toelf-requirement">
                                         <span class="toelf-requirement">托福: {{ $college->toeflRequirement('本科') }}</span>
-                                        <span class="ielts-requirement">雅思: {{ $college->ieltsRequirement('本科') }}</span>
+                                        <span class="ielts-requirement" style="margin-left: 20px">雅思: {{ $college->ieltsRequirement('本科') }}</span>
                                     </div>
 
                                     <div class="address-container">
