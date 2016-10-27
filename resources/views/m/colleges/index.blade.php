dc96912949aef2c871dceb499b8529cf2a6fa60b
hujianchuang <hujianchuang@192.168.1.32>
2016-10-28 01:00:51 +0800

    修改院校查询 学校概况等!

--- resources/views/m/colleges/index.blade.php
+++ resources/views/m/colleges/index.blade.php
@@ -3,19 +3,48 @@
 <form action="" method="get" id="search">
 <div class="main02" id="main02">
     <div class="yuanxiao_cx_main">
-        <div class="yx_chaxun" style="margin:2% auto;">
+        <div class="yx_chaxun" style="margin:2% 2%;float:left; width:78%;border-radius: 5px;">
             
                 <input type="text" class="chax_input" name="college_name" value="{{$college_name}}" placeholder="院校查询">
                 <input type="hidden" name="selected_country_id" value="-1"/>
                 <input type="submit" class="chax_so" value="">
             
         </div>
-
+        <div class="shaixuan_icon">
+            <img src="/static/images/shaixuan.png" height="65" onclick="shaixuan('.shaixuan')" />
+        </div>
+        <div class="clear"></div>
     </div>
-    <div class="chaxun01"><a href="javascript:shaixuan('.shaixuan')"><span>•</span>条件筛选</a></div>
+    <!--<div class="chaxun01"><a href="javascript:shaixuan('.shaixuan')"><span>•</span>条件筛选</a></div>-->
     <div class="chaxun02"><h1>您找到<span>{{ $colleges->total() }}</span>所相关学校</h1></div>
     <div class="grzy_wdsc_list">
+         @foreach($colleges as $college)
+            <div class="pinggu_xx50" >
+                <div class="pinggu_xx_name50">
+                    <a href="{{route('colleges.show', $college->key)}}" ><h2>
+                        <img src="{{app('qiniu_uploader')->pathOfKey($college->badge_path)}}"><br />
+                        <span style="display:block; float:left;">{{$college->chinese_name}}</span><span style="background:#23e6bb;display:block; float:left; color:#fff; border-radius:3px; padding:1% 2%; font-size:0.8em; margin:0 0 0 5px;">{{ ($college->type=="public")?'公立':'私立' }}</span><br />
+                        <div class="clear"></div>
+                    {{$college->english_name}}
+                        </h2></a>
+                        <h1>本国排名：{{$college->domestic_ranking}}<br><span style="background:url(/static/images/icon21.jpg) left no-repeat; background-size:20px; padding:0 0 0 20px;">{{$college->administrativeArea->name}}
+                                            @if($college->administrativeArea->parent)
+                                                , {{$college->administrativeArea->parent->name}}
+                                                @if($college->administrativeArea->parent->parent)
+                                                    , {{$college->administrativeArea->parent->parent->name}}
+                                                @endif
+                                            @endif</span><br>
+                            <a href="/estimate/step-1?selected_country_id={{$college->country_id}}&college_id={{$college->id}}" style="font-size:1.2em; line-height: 40px;">测试录取率>></a><br>
+                       
+                            <img src="/static/images/xin<?php if(app('auth')->user()){ if(app('auth')->user()->isLikeCollege($college->id)){echo 1;} else {echo 2;}}else{echo 2;} ?>.png" width="30" style=" cursor: pointer;" likeid='<?php if(app('auth')->user()){ if(app('auth')->user()->isLikeCollege($college->id)){echo 1;} else {echo 2;}}else{echo 3;} ?>' onclick="setLike('{{ $college->id }}',$(this))" ><span id='shuzi{{ $college->id }}'>{{ $college->like_nums }}</span>
+                       </h1>
+                       <div class="clear"></div>
+                </div>     
+            </div>
+        @endforeach
+        <!--
         <ul>
+           
             @foreach($colleges as $college)
             <a href="{{route('colleges.show', $college->key)}}" ><li>
                     <img src="{{app('qiniu_uploader')->pathOfKey($college->badge_path)}}" style="display: block; margin: 0 auto;">
@@ -23,7 +52,7 @@
                 </li></a>
            @endforeach
 
-        </ul>
+        </ul>-->
     </div>
     <div class="clear"></div>
     <div class='page'>
