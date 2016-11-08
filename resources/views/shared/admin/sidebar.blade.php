d18d939b8eebe17132ae178b4e0390c53b74ec3b
ChengLei Shao <chareice@live.com>
2016-11-08 12:19:54 +0800

    修改后台链接设置

--- resources/views/shared/admin/sidebar.blade.php
+++ resources/views/shared/admin/sidebar.blade.php
@@ -33,53 +33,83 @@
               </span>
             </a>
           </li>
-          <li class="nav-main-heading"><span class="sidebar-mini-hide">系统管理</span></li>
+
           <li>
-            <a href="{{ route('admin.users.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">用户管理</span></a>
+            <a class="nav-submenu" data-toggle="nav-submenu" href="#">
+              <i class="si si-badge"></i><span class="sidebar-mini-hide">首页设置</span></a>
+            <ul>
+              <li>
+                <a href="{{ route('admin.setting.index', ['key' => 'use_people_nums']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">使用人数设置</span></a>
+              </li>
+
+              <li>
+                <a href="{{ route('admin.setting.index', ['key' => 'abroad_people_nums']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">留学人数设置</span></a>
+              </li>
+
+              <li>
+                <a href="{{ route('admin.setting.index', ['key' => 'index_more']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">首页“更多”链接设置</span></a>
+              </li>
+
+              <li>
+                <a href="{{ route('admin.articles.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">首页文章-页面管理</span></a>
+              </li>
+            </ul>
+          </li>
+
+
+          <li>
+            <a class="nav-submenu" data-toggle="nav-submenu" href="#">
+              <i class="si si-badge"></i><span class="sidebar-mini-hide">用户设置</span></a>
+            <ul>
+              <li>
+                <a href="{{ route('admin.users.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">用户管理</span></a>
+              </li>
+              <li>
+                <a href="{{ route('admin.intentions.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">用户意向管理</span></a>
+              </li>
+            </ul>
           </li>
 
           <li>
-              <a href="{{ route('admin.setting.index', ['key' => 'core_range']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">核心冲刺概率设置</span></a>
-          </li>
+            <a class="nav-submenu" data-toggle="nav-submenu" href="#">
+              <i class="si si-badge"></i><span class="sidebar-mini-hide">院校信息设置</span></a>
+            <ul>
+                <li>
+                  <a class="nav-submenu" data-toggle="nav-submenu" href="#">
+                    <i class="si si-badge"></i><span class="sidebar-mini-hide">院校管理</span></a>
+                  <ul>
+                    <li>
+                      <a href="{{ route('admin.colleges.index') }}"><i class="si si-rocket"></i>
+                        <span class="sidebar-mini-hide">院校列表</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="{{ route('admin.colleges.create') }}"><i class="si si-rocket"></i>
+                        <span class="sidebar-mini-hide">新增院校</span>
+                      </a>
+                    </li>
+                  </ul>
+              </li>
+              <li>
+                <a href="{{ route('admin.setting.index', ['key' => '985list']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">985院校列表设置</span></a>
+              </li>
 
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => '985list']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">985院校列表设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => '211list']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">211院校列表设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => 'master_colleges']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">硕士-最近就读学院设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => 'master_speciality']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">硕士-最近就读专业设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => 'use_people_nums']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">使用人数设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => 'abroad_people_nums']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">留学人数设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.setting.index', ['key' => 'index_more']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">首页“更多”链接设置</span></a>
+              <li>
+                <a href="{{ route('admin.setting.index', ['key' => '211list']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">211院校列表设置</span></a>
+              </li>
+              <li>
+                  <a href="{{ route('admin.setting.index', ['key' => 'core_range']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">核心冲刺概率设置</span></a>
+              </li>
+              <li>
+                <a href="{{ route('admin.examination_score_weights.index') }}"><i class="si si-rocket"></i>
+                  <span class="sidebar-mini-hide">分数比例设置</span>
+                </a>
+              </li>
+            </ul>
           </li>
 
           <li>
             <a href="{{ route('admin.setting.index', ['key' => 'rankings']) }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">排行榜设置</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.intentions.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">用户意向管理</span></a>
-          </li>
-
-          <li>
-            <a href="{{ route('admin.articles.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">首页文章-页面管理</span></a>
           </li>
 
           <li>
@@ -88,39 +118,6 @@
 
           <li>
             <a href="{{ route('admin.messages.index') }}"><i class="si si-rocket"></i><span class="sidebar-mini-hide">站内通知管理</span></a>
-          </li>
-          <li>
-            <a class="nav-submenu" data-toggle="nav-submenu" href="#">
-              <i class="si si-badge"></i><span class="sidebar-mini-hide">院校管理</span></a>
-            <ul>
-              <li>
-                <a href="{{ route('admin.colleges.index') }}"><i class="si si-rocket"></i>
-                  <span class="sidebar-mini-hide">院校列表</span>
-                </a>
-              </li>
-              <li>
-                <a href="{{ route('admin.colleges.create') }}"><i class="si si-rocket"></i>
-                  <span class="sidebar-mini-hide">新增院校</span>
-                </a>
-              </li>
-            </ul>
-        </li>
-
-          <li>
-            <a class="nav-submenu" data-toggle="nav-submenu" href="#">
-              <i class="si si-badge"></i><span class="sidebar-mini-hide">分数比例设置</span></a>
-            <ul>
-              <li>
-                <a href="{{ route('admin.examination_score_weights.index') }}"><i class="si si-rocket"></i>
-                  <span class="sidebar-mini-hide">比例列表</span>
-                </a>
-              </li>
-              <li>
-                <a href="{{ route('admin.examination_score_weights.create') }}"><i class="si si-rocket"></i>
-                  <span class="sidebar-mini-hide">新增规则</span>
-                </a>
-              </li>
-            </ul>
           </li>
         </ul>
       </div>
